bfd1b88da6a9c9da46c18778084d105cfc1d7660
BryanPatucci <bryanpatuccisilva2002@gmail.com>
2019-08-27 11:57:00 +0900

    Foi feita e terminada a parte de design, funcionamento do js e de outros dispositivos do leitor de código de barras, assim como foi deixado em fullscreen para uma maior imersão.

--- www/components/js/sistema.js
+++ www/components/js/sistema.js
@@ -1,33 +1,39 @@
 function Leitor(){
   cordova.plugins.barcodeScanner.scan(
     function(result){
-    if(result.text = 280720550){
+    if(result.text == 280720550){
       window.location.href = "disp1.html";
       alert("Leitura do Código de barras\n" +
       "Resultado" + result.text + "\n" +
       "Formato" + result.format + "\n" +
       "Cancelado" + result.cancelled);
     }
-    else if(result.text = 989895555){
+    else if(result.text == 989895555){
       window.location.href = "disp2.html";
       alert("Leitura do Código de barras\n" +
       "Resultado" + result.text + "\n" +
       "Formato" + result.format + "\n" +
       "Cancelado" + result.cancelled);
     }
-    else if(result.text = 85236987){
+    else if(result.text == 85236987){
       window.location.href = "disp3.html";
       alert("Leitura do Código de barras\n" +
       "Resultado" + result.text + "\n" +
       "Formato" + result.format + "\n" +
       "Cancelado" + result.cancelled);
     }
-    else if(result.text = 85369877444){
+    else if(result.text == 85369877444){
       window.location.href = "disp4.html";
       alert("Leitura do Código de barras\n" +
       "Resultado" + result.text + "\n" +
       "Formato" + result.format + "\n" +
       "Cancelado" + result.cancelled);
+    }
+      if(result.text != 280720550 && result.text != 989895555 && result.text != 85236987 && result.text != 85369877444) {
+      alert("Código não identificado !\nLeitura do código  de barras\n" +
+        "Resultado: " + result.text + "\n" +
+        "Formato: " + result.format+ "\n" +
+        "Cancelado: " + result.cancelled);
     }
     },
     function (error){
